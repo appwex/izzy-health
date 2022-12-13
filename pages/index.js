acff9bbe044173bc90ea15fc3d57226c5d3e3cf2
mrxshinji <95459307+mrxshinji@users.noreply.github.com>
2022-12-13 13:23:09 +0800

    add head

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
+import Head from 'next/head'
+
 const Home = () => {
   return (
     <div className="mx-auto flex h-screen items-center justify-center">
+      <Head>
+        <title>Izzy-Health</title>
+        <meta
+          name="description"
+          content="Personalized health for you and your family from anywhere"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <h1 className="text-3xl font-bold underline ">Hello world!</h1>
     </div>
   )
--- pages/test/test-page/index.jsx
+++ pages/test/test-page/index.jsx
@@ -1,8 +1,14 @@
 import Test from 'components/test/test'
+import Head from 'next/head'
 
 const TestStory = () => {
   return (
     <div className="mx-auto flex h-full min-h-full items-center justify-center">
+      <Head>
+        <title>Izzy-Health Test Place</title>
+        <meta name="description" content="place to test your components" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Test />
     </div>
   )
