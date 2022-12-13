9d1514142c527f980cf9a67b6dd91d79b43a308e
mrxshinji <95459307+mrxshinji@users.noreply.github.com>
2022-12-13 13:11:15 +0800

    minor fix

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 const Home = () => {
   return (
-    <div className="mx-auto flex items-center justify-center h-screen ">
+    <div className="mx-auto flex h-screen items-center justify-center">
       <h1 className="text-3xl font-bold underline ">Hello world!</h1>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
---
+++ pages/test/test-page/index.jsx
@@ -0,0 +1,11 @@
+import Test from 'components/test/test'
+
+const TestStory = () => {
+  return (
+    <div className="mx-auto flex h-full min-h-full items-center justify-center">
+      <Test />
+    </div>
+  )
+}
+
+export default TestStory
