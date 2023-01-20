a353d9a0082e49eaf1cf529d8c2fc3611471dd7c
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-01-20 15:58:12 +0530

    Landig  page completed. Needs two more section.

--- pages/landing/index.js
+++ pages/landing/index.js
@@ -1,5 +1,9 @@
 import Cta from 'components/cta'
 import Navbar from '../../src/components/navbar/navbar'
+import CareSection from '../stories/care-section'
+import Combat from '../stories/combat'
+import Dontneed from '../stories/dontneed'
+import Footer from '../stories/footer'
 
 import stl from './Landing.module.scss'
 
@@ -7,6 +11,10 @@ const Landing = () => (
   <div className={stl.wrapper}>
     <Navbar />
     <Cta />
+    <CareSection />
+    <Combat />
+    <Dontneed />
+    <Footer />
   </div>
 )
 
--- src/components/careSection/careSection.js
+++ src/components/careSection/careSection.js
@@ -2,6 +2,8 @@ import clsx from 'clsx'
 import stl from './careSection.module.scss'
 import Link from 'next/link'
 import Button from 'components/button'
+import IzzyButton from 'components/izzyButton'
+import { BUTTON_TYPE } from 'components/izzyButton/izzyButton'
 import { Nutritionist, Physician, Psychologist } from 'assets/svg'
 
 const CareSection = () => {
@@ -15,14 +17,19 @@ const CareSection = () => {
         <div className={stl.body}>
           <div className={stl.text}>
             <p className={clsx(stl.contact)}>
-              Contact our Doctor for the first time <span>Free </span>
-              <Link
-                href={`#start`}
-                passHref
-              >
-                <Button label="Let's start" btnType='rounded'/>
-              </Link>
+              Contact our Doctor for the first time
             </p>
+            <div className={stl.start}>
+              <span>Free</span>
+              <Link href={`#start`} passHref>
+                <IzzyButton
+                  varient={BUTTON_TYPE.btnPrimary}
+                  btnType={BUTTON_TYPE.btnRounded}
+                  label={"Let's start"}
+                />
+              </Link>
+            </div>
+
             <p className={stl.compare}>
               Lets compare our service pricing with a few countries we serve
             </p>
