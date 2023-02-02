25fd223d586bbffbb5b7ae92eb67c31f8f84c258
NadiaFrShLm <84836971+NadiaFrShLm@users.noreply.github.com>
2023-02-02 13:05:36 +0100

    fix up the .svg flags issue

--- src/components/comparePricing/comparePricing.js
+++ src/components/comparePricing/comparePricing.js
@@ -4,8 +4,6 @@ import clsx from 'clsx'
 import PricingCard from './pricingCard'
 import rawData from './rawData'
 
-import { India, Mexico, Nigeria } from './assets'
-
 const ComparePricing = () => {
   const [element, setData] = useState(rawData[0])
 
@@ -13,16 +11,22 @@ const ComparePricing = () => {
     const parentToChild = () => {
       setData(element)
     }
+
     return (
       <div
         key={element.index}
         className={clsx(stl.country)}
-        onClick={() => parentToChild(element.country)}
+        onClick={() => {
+          parentToChild(element.country)
+        }}
       >
         {/* flag icon */}
-        <India />
-        {/* <img src={`./assets/${element.flagIcon}`} />  DOESNT'T WORK */}
-
+        <img
+          src={`../../assets/${element.flagIcon}`}
+          alt="icon"
+          // width={50}
+          // height={50}
+        />
         {/* country name */}
         <span>{element.country}</span>
       </div>
@@ -34,6 +38,7 @@ const ComparePricing = () => {
       <div className={stl.pricing}>
         {/* list of countries (flag and name) */}
         <div className={stl.countries}>{countryData}</div>
+
         {/* pricing card dinamically changing */}
         <PricingCard {...element} />
       </div>
@@ -42,50 +47,3 @@ const ComparePricing = () => {
 }
 
 export default ComparePricing
-
-{
-  /* default values
-          <div
-            className={clsx(
-              stl.country 
-              // data.country === 'Nigeria' ? stl.active : ''
-            )}
-            onClick={() => parentToChild('Nigeria')}
-          >
-            <Nigeria />
-            <span>Nigeria</span>
-          </div>
-     
-          <div
-            className={clsx(stl.country)}
-            onClick={() => parentToChild('India')}
-          >
-            <India />
-            <span>India</span>
-          </div>
-          <div className={stl.country} onClick={() => parentToChild('Mexico')}>
-            <Mexico />
-            <span>Mexico</span>
-          </div>
-          <div
-            className={stl.country}
-            onClick={() => parentToChild('CostaRica')}
-          >
-            <CostaRica />
-            <span>Costa Rica</span>
-          </div>
-          <div
-            className={stl.country}
-            onClick={() => parentToChild('Colombia')}
-          >
-            <Colombia />
-            <span>Colombia</span>
-          </div>
-          <div
-            className={stl.country}
-            onClick={() => parentToChild('Dominican')}
-          >
-            <Dominican />
-            <span>Dominican Republic</span>
-          </div>*/
-}
